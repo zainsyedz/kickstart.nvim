7d8821b7d6638dfead9c3ace39195c1d43a1c251
Zain Syed <zainalabidin.syed@live.com>
2024-12-14 21:55:15 +0530

    Added intelephense settings

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -556,6 +556,15 @@ require('lazy').setup({
             },
           },
         },
+        intelephense = {
+          capabilities = vim.lsp.protocol.make_client_capabilities(),
+          settings = {
+            intelephense = {
+              format = { enable = true },
+              diagnostics = { enable = true },
+            },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
@@ -852,7 +861,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
